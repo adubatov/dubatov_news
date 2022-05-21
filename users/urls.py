72eb4d4549880742106c47b48165b0f725959f79
adubatov <andrey.dubatov@gmail.com>
2022-05-21 20:21:23 +0300

    Profile view and update enhanced

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView, UserDetailView
+from .views import SignUpView, UserDetailView, UserUpdateView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('<int:pk>/update', UserUpdateView.as_view(), name='user_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
-from django.urls import reverse_lazy
-from django.contrib.auth import get_user_model
+from django.views.generic import CreateView, DetailView, UpdateView
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth import get_user_model, get_user
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from requests import request
 
 from .forms import CustomUserCreationForm
 
@@ -18,3 +20,23 @@ class UserDetailView(DetailView):
     model = get_user_model()
     template_name = 'user_detail.html'
     context_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['auth_user'] = get_user(self.request)
+        return context
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'user_update.html'
+    context_name = 'user'
+    login_url = 'login'
+    fields = ['first_name', 'last_name', 'email', 'age']
+
+    def test_func(self):
+        user = self.get_object()
+        return self.request.user == user
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('user_detail', kwargs={'pk':self.request.user.pk})
